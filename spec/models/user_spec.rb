3e472a4d128900c02d288c519b7fd34a1e483f8c
Tri Pham <tdpham3@gmail.com>
2014-08-14 10:58:22 -0700

    User can now sign in and out

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token)}
   it { should respond_to(:authenticate)}
 
 
@@ -21,14 +22,17 @@ describe User do
     before { @user.name = " " }
     it { should_not be_valid }
   end	
+
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid}
+  end 
+
   describe "when email is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
   end
-  describe "when name is too long" do
-    before { @user.name = "a" * 51 }
-    it { should_not be_valid}
-  end
+
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
@@ -62,14 +66,14 @@ describe User do
 
   describe "when password is not present" do
     before do
-      @user = User.new(name: "Example User", email: "user@example.com",
+      @user = User.new(name: "Jax The Destroyer", email: "jax@example.com",
                        password: " ", password_confirmation: " ")
     end
     it { should_not be_valid }
   end
 
   describe "when password doesn't match confirmation" do
-    before { @user.password_confirmation = "mismatch" }
+    before { @user.password_confirmation = "starprince" }
     it { should_not be_valid }
   end
 
@@ -92,6 +96,11 @@ describe User do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 
 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -33,8 +33,8 @@ describe "User pages" do
 
     describe "with valid information" do
       before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
+        fill_in "Name",         with: "Super Skrull"
+        fill_in "Email",        with: "skrull@example.com"
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
@@ -42,6 +42,16 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'skrull@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+
     end
   end
   
